298c1c11d52812fc9e8dce087850dc2322d3dcb7
Tommy Messbauer <totally.bro@gmail.com>
2014-04-23 22:38:26 -0500

    added ability to writeHead() before stream object

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ var _ = require('lodash');
 module.exports = function(namespace) {
   namespace = namespace || 'json';
 
-  this.attach = function (options) {
+  this.attach = function(options) {
     var jsonpEnabled = options.jsonpEnabled === true || options.jsonpEnabled === 'true' ? true : false;
 
     /**
-    * Takes the given model and generated the main object that will be passed to the rendering engine.
-    * This will include the utils, underscore, siteConfig, and other basic rendering constructs.
-    **/
+     * Takes the given model and generated the main object that will be passed to the rendering engine.
+     * This will include the utils, underscore, siteConfig, and other basic rendering constructs.
+     **/
     /**
-    * Takes the given model and generated the main object that will be passed to the rendering engine.
-    * This will include the utils, underscore, siteConfig, and other basic rendering constructs.
-    * @param res {HttpServerResponse} the res to write the output.
-    * @param data {Object} Data to pass to the rendering view
-    * @param data {Object} Keys/values for headers (This action will set the Content_Type: 'application/json' automatically)
-    **/
+     * Takes the given model and generated the main object that will be passed to the rendering engine.
+     * This will include the utils, underscore, siteConfig, and other basic rendering constructs.
+     * @param res {HttpServerResponse} the res to write the output.
+     * @param data {Object} Data to pass to the rendering view
+     * @param data {Object} Keys/values for headers (This action will set the Content_Type: 'application/json' automatically)
+     **/
     this[namespace] = function(data, res, headers, req) {
       var str = JSON.stringify(data || {});
-      
-      res.writeHead(200, _.extend({ 'Content-Type': 'application/json'}, headers) );
+
+      if (!res.headersSent) {
+        res.writeHead(200, _.extend({
+          'Content-Type': 'application/json'
+        }, headers));
+      }
 
       if (jsonpEnabled && req && req.query) {
         var cb = req.query.callback ? req.query.callback : req.query.jsonp;
@@ -29,7 +33,7 @@ module.exports = function(namespace) {
           str = cb + '(' + str + ')';
         }
       }
-      
+
       res.end(str);
     };
 
